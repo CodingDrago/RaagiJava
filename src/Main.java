2908d35168deb704afab12cda7e39a9668b73cb3
CodingDrago <gunasai243@gmail.com>
2026-02-13 10:28:07 +0530

    Initial lab commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
+
+import java.util.Scanner;
+
+public class AddNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+        System.out.println("Sum = " + (a + b));
+    }
+}
+
+
+
