a5179bf8c8141e9c0e35343027c199086d5558dc
CodingDrago <gunasai243@gmail.com>
2026-02-13 10:59:16 +0530

    Added Factorial.java which will give us factorial of a given number

--- src/Factorial.java
+++ src/Factorial.java
@@ -1,6 +1,6 @@
 public class FactorialIterative {
     public static void main(String[] args) {
-        int number = 5; // Example input
+        int number = 5;
         long factorial = 1;
 
         for (int i = 1; i <= number; i++) {
