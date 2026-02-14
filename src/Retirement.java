223216306b9b8e8bd9841d1bdb560272b8d9c0bf
CodingDrago <gunasai243@gmail.com>
2026-02-14 10:01:07 +0530

    Added Retirement.java which computes and display output and display as below: the number of years left until retirement the calendar year in which retirement will occur based on system's current year if the user is already past the desired retirement age, the program shoulkd notify them that they are eligible to retire

--- src/Retirement.java
+++ src/Retirement.java
@@ -4,27 +4,20 @@ import java.util.Scanner;
 public class Retirement {
 
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
-
         System.out.print("What is your current age? ");
         int currentAge = scanner.nextInt();
-
         System.out.print("At what age would you like to retire? ");
         int retirementAge = scanner.nextInt();
-
         int currentYear = Year.now().getValue();
         int yearsLeft = retirementAge - currentAge;
-
         if (yearsLeft > 0) {
             int retirementYear = currentYear + yearsLeft;
-
             System.out.println("\nYou have " + yearsLeft + " years left until you can retire.");
             System.out.println("It's " + currentYear + ", so you can retire in " + retirementYear + ".");
         } else {
             System.out.println("\nYou are already eligible to retire!");
         }
-
         scanner.close();
     }
 }
